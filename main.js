244458cc44b24b6376a19ca4f1c51ec9d3d26cc3
Preadator/Dukman <alexalduk@hotmail.com>
2021-01-29 21:27:23 +1100

    Fixed gallery card image display Need to fix the layout of the content section.

--- main.js
+++ main.js
@@ -1,24 +1,35 @@
 function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   console.log("triggered scroll");
-  }
+}
 
-  // document.querySelector('.rttFlower').addEventListener('click', () => {
-  //   document.body.scrollTop = 0; // For Safari
-  //   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-  // console.log("triggered scroll");
-  // })
+// document.querySelector('.rttFlower').addEventListener('click', () => {
+//   document.body.scrollTop = 0; // For Safari
+//   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+// console.log("triggered scroll");
+// })
 
-  // Aggression cart events
-  const aggressionCard = document.querySelector('.aggressionCard');
-  aggressionCard.addEventListener('click', () => {
-    console.log("aggression card clicked");
-  })
+// Aggression cart events
+const aggressionCard = document.querySelector('.aggressionCard');
+aggressionCard.addEventListener('click', () => {
+  console.log("aggression card clicked");
+});
 
-  new Glide('.glide', {
-    type: 'carousel',
-    autoplay: 4000 | true,
-    hoverPause: true,
-    animationDuration: 2000
-}).mount()
+new Glide('.glide', {
+  type: 'carousel',
+  autoplay: 4000 | true,
+  hoverPause: true,
+  animationDuration: 2000
+}).mount();
+
+const imageCard = document.querySelector('.card-basic')
+imageCard.addEventListener('mouseover', () => {
+  console.log("anything");
+});
+
+
+let test = document.querySelector('.card-basic')
+test.addEventListener("click", () => {
+  console.log("You knocked?");
+});
