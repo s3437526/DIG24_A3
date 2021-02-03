aea5ee30613a8952a38354c7c4237b0ffe4d59c5
Preadator/Dukman <alexalduk@hotmail.com>
2021-02-03 14:33:07 +1100

    Fixed content headings, added more animation detail to WHAT IS DEMENTIA section

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-    console.log("triggered scroll");
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  console.log("triggered scroll");
 }
 
 // document.querySelector('.rttFlower').addEventListener('click', () => {
@@ -13,38 +13,55 @@ function topFunction() {
 // Aggression cart events
 const dementiaTypeCard = document.querySelector('.dementiaTypeCard');
 dementiaTypeCard.addEventListener('click', () => {
-    console.log("dementiaTypeCard card clicked");
+  console.log("dementiaTypeCard card clicked");
 });
 
 new Glide('.glide', {
-    type: 'carousel',
-    autoplay: 8000 | true,
-    hoverPause: true,
-    animationDuration: 2000
+  type: 'carousel',
+  autoplay: 8000 | true,
+  hoverPause: true,
+  animationDuration: 2000
 }).mount();
 
 const imageCard = document.querySelector('.card-basic')
 imageCard.addEventListener('mouseover', () => {
-    console.log("anything");
+  console.log("anything");
 });
 
 
 let test = document.querySelector('.card-basic')
 test.addEventListener("click", () => {
-    console.log("You knocked?");
+  console.log("You knocked?");
 });
 
 document.querySelector('.dementiaFact').addEventListener('click', () => {
-    console.log("clicked")
+  console.log("clicked")
 });
 
 // GSAP animate WHAT IS DEMENTIA section
 const whatIsDementiaTl = gsap.timeline({
-    scrollTrigger: {
-        trigger: "#trigger1",
-        start: "top top",
-        scrub: 1
-    }
-})
-whatIsDementiaTl.to('#fact1', { autoAlpha: 1, opacity: 1, display: "flex" });
-whatIsDementiaTl.to('#fact2', 5, { autoAlpha: 1, opacity: 1, display: "flex" });
+  scrollTrigger: {
+    trigger: "#trigger1",
+    start: "top top",
+    scrub: 1,
+    end: "+=8000",
+    // end: "+=" + (window.innerHeight * 2),
+    pin: true // this breaks svg transparency
+  } // looks like it is the opacity of the parallax that is effecting this
+}) // can be seen on chrome dev tools on iphone4 setting
+whatIsDementiaTl.to('#fact1', 10, { autoAlpha: 1, opacity: 1, display: "flex" });
+whatIsDementiaTl.to('#fact2', 10, { autoAlpha: 1, opacity: 1, display: "flex" });
+whatIsDementiaTl.to('#fact3', 10, { autoAlpha: 1, opacity: 1, display: "flex" });
+whatIsDementiaTl.to('#fact4', 10, { autoAlpha: 1, opacity: 1, display: "flex" });
+whatIsDementiaTl.to('#fact5', 10, { autoAlpha: 1, opacity: 1, display: "flex" });
+whatIsDementiaTl.to('#fact6', 10, { autoAlpha: 1, opacity: 1, display: "flex" });
+whatIsDementiaTl.to('#fact7', 10, { autoAlpha: 1, opacity: 1, display: "flex" });
+
+whatIsDementiaTl.to(".dementiaFact", 5, { opacity: 0, display: "none" });
+
+whatIsDementiaTl.to('#fact8', 10, { autoAlpha: 1, opacity: 1, display: "flex" });
+whatIsDementiaTl.to('#fact9', 10, { autoAlpha: 1, opacity: 1, display: "flex" });
+whatIsDementiaTl.to('#fact10', 10, { autoAlpha: 1, opacity: 1, display: "flex" });
+whatIsDementiaTl.to('#fact11', 10, { autoAlpha: 1, opacity: 1, display: "flex" });
+whatIsDementiaTl.to('#fact12', 10, { autoAlpha: 1, opacity: 1, display: "flex" });
+whatIsDementiaTl.to('#fact13', 10, { autoAlpha: 1, opacity: 1, display: "flex" });
