2192d8d6b23b97036f666aa569877def020cafb4
Alex <alexalduk@hotmail.com>
2021-02-16 19:34:40 +1100

    Added bouncing dropdown arrow for scrolling ind.

--- main.js
+++ main.js
@@ -1,35 +1,40 @@
+// Back to top function
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
+// Carousel CSS animation
 new Glide('.glide', {
-  type: 'carousel',
-  autoplay: 8000 | true,
-  hoverPause: true,
-  animationDuration: 2000
+    type: 'carousel',
+    autoplay: 8000 | true,
+    hoverPause: true,
+    animationDuration: 2000
 }).mount();
 
+// Register GSAP complenents
+gsap.registerPlugin(MotionPathPlugin);
 gsap.registerPlugin(ScrollTrigger);
 
 // GSAP animate WHAT IS DEMENTIA section
 const whatIsDementiaTl = gsap.timeline({
-  scrollTrigger: {
-    trigger: "#trigger1",
-    start: "top top",
-    scrub: 1,
-    markers: true,
-    anticipatePin: 1,
-    end: "+=20000",
-    // end: "+=" + (window.innerHeight * 2),
-    pin: true // this breaks svg transparency
-  } // looks like it is the opacity of the parallax that is effecting this
-}) // can be seen on chrome dev tools on iphone4 setting
+        scrollTrigger: {
+            trigger: "#trigger1",
+            start: "top top",
+            scrub: 1,
+            markers: true,
+            anticipatePin: 1,
+            end: "+=20000",
+            // end: "+=" + (window.innerHeight * 2),
+            pin: true // this breaks svg transparency
+        } // looks like it is the opacity of the parallax that is effecting this
+    }) // can be seen on chrome dev tools on iphone4 setting
 whatIsDementiaTl.to('#fact1', 300, { autoAlpha: 1, opacity: 1, display: "flex" });
 whatIsDementiaTl.to('#fact2', 300, { autoAlpha: 1, opacity: 1, display: "flex" });
 whatIsDementiaTl.to('#fact3', 300, { autoAlpha: 1, opacity: 1, display: "flex" });
 whatIsDementiaTl.to('#fact4', 300, { autoAlpha: 1, opacity: 1, display: "flex" });
 whatIsDementiaTl.to('#fact5', 300, { autoAlpha: 1, opacity: 1, display: "flex" });
+whatIsDementiaTl.to('.downArrow', 300, { autoAlpha: 1, opacity: 0, display: "none" });
 whatIsDementiaTl.to('#fact6', 300, { autoAlpha: 1, opacity: 1, display: "flex" });
 whatIsDementiaTl.to('#fact7', 300, { autoAlpha: 1, opacity: 1, display: "flex" });
 
@@ -37,6 +42,7 @@ whatIsDementiaTl.to(".dementiaFact", 50, { opacity: 0, autoAlpha: 0 });
 whatIsDementiaTl.to("#factsSlide1", 50, { opacity: 0, display: "none" });
 
 whatIsDementiaTl.to('#factsSlide2', 10, { autoAlpha: 1, opacity: 1, display: "block" });
+whatIsDementiaTl.to('.downArrow', 300, { autoAlpha: 1, opacity: 1, display: "block" });
 
 whatIsDementiaTl.to('#fact8', 300, { autoAlpha: 1, opacity: 1, display: "flex" });
 whatIsDementiaTl.to('#fact9', 300, { autoAlpha: 1, opacity: 1, display: "flex" });
@@ -54,26 +60,25 @@ whatIsDementiaTl.to('#commonType2', 500, { autoAlpha: 1, opacity: 1, display: "f
 whatIsDementiaTl.to('#commonType3', 500, { autoAlpha: 1, opacity: 1, display: "flex" });
 whatIsDementiaTl.to('#commonType4', 500, { autoAlpha: 1, opacity: 1, display: "flex" });
 
-gsap.registerPlugin(MotionPathPlugin);
 
 const whatIsDementiaT2 = gsap.timeline({
-  scrollTrigger: {
-    trigger: "#trigger1",
-    start: 1000,
-    scrub: 1,
-    end: "+=2000",
-    yoyo: true,
-    anticipatePin: 1,
-    ease: "power1.inOut",
-    motionPath: {
-      path: "#path",
-      align: "#placeholder1",
-      autoRotate: true,
-      alignOrigin: [0.5, 0.5],
-      // end: "+=" + (window.innerHeight * 2),
-      pin: true // this breaks svg transparency
-    }
-  } // looks like it is the opacity of the parallax that is effecting this
+    scrollTrigger: {
+        trigger: "#trigger1",
+        start: 1000,
+        scrub: 1,
+        end: "+=2000",
+        yoyo: true,
+        anticipatePin: 1,
+        ease: "power1.inOut",
+        motionPath: {
+            path: "#path",
+            align: "#placeholder1",
+            autoRotate: true,
+            alignOrigin: [0.5, 0.5],
+            // end: "+=" + (window.innerHeight * 2),
+            pin: true // this breaks svg transparency
+        }
+    } // looks like it is the opacity of the parallax that is effecting this
 })
 
 // Common dementia type 1 dialog
@@ -86,17 +91,17 @@ closeButton1.addEventListener('click', () => dementiaDialog1.hide());
 
 // Dementia among us animation
 const dementiaPrevelanceT1 = gsap.timeline({
-  scrollTrigger: {
-    trigger: "#trigger3",
-    start: "top top",
-    scrub: 1,
-    markers: true,
-    anticipatePin: 1,
-    end: "+=15000",
-    // end: "+=" + (window.innerHeight * 2),
-    pin: true // this breaks svg transparency
-  } // looks like it is the opacity of the parallax that is effecting this
-}) // can be seen on chrome dev tools on iphone4 setting
+        scrollTrigger: {
+            trigger: "#trigger3",
+            start: "top top",
+            scrub: 1,
+            markers: true,
+            anticipatePin: 1,
+            end: "+=15000",
+            // end: "+=" + (window.innerHeight * 2),
+            pin: true // this breaks svg transparency
+        } // looks like it is the opacity of the parallax that is effecting this
+    }) // can be seen on chrome dev tools on iphone4 setting
 
 dementiaPrevelanceT1.to('.prevelanceContainers', 10, { autoAlpha: 1, opacity: 1, display: "block" });
 
@@ -115,6 +120,7 @@ dementiaPrevelanceT1.to('#symptom3', 300, { autoAlpha: 1, opacity: 1, display: "
 dementiaPrevelanceT1.to('#symptom4', 300, { autoAlpha: 1, opacity: 1, display: "flex" });
 dementiaPrevelanceT1.to('#symptom5', 300, { autoAlpha: 1, opacity: 1, display: "flex" });
 dementiaPrevelanceT1.to('#symptom6', 300, { autoAlpha: 1, opacity: 1, display: "flex" });
+dementiaPrevelanceT1.to('.downArrow', 300, { autoAlpha: 1, opacity: 0, display: "none" });
 dementiaPrevelanceT1.to('#symptom7', 300, { autoAlpha: 1, opacity: 1, display: "flex" });
 dementiaPrevelanceT1.to('#symptom8', 300, { autoAlpha: 1, opacity: 1, display: "flex" });
 dementiaPrevelanceT1.to('#symptom9', 300, { autoAlpha: 1, opacity: 1, display: "flex" });
@@ -128,60 +134,53 @@ const duration = 1;
 // 4 common dementia types animations
 const dementiaCard = gsap.utils.toArray(".card-overview");
 dementiaCard.forEach((card, i) => {
-  card.addEventListener("mouseenter", (e) => {
-    gsap.to(e.target, duration / 4, { y: -10, scale: 1.2 });
-    gsap.to(e.target, { boxShadow: "0 0 20px rgba(0,0,0,0.06)" });
-  });
+    card.addEventListener("mouseenter", (e) => {
+        gsap.to(e.target, duration / 4, { y: -10, scale: 1.2 });
+        gsap.to(e.target, { boxShadow: "0 0 20px rgba(0,0,0,0.06)" });
+    });
 
-  card.addEventListener("mouseleave", (e) => {
-    gsap.to(e.target, duration / 2, { ease: Bounce.easeOut, y: 0, scale: 1 });
-  });
+    card.addEventListener("mouseleave", (e) => {
+        gsap.to(e.target, duration / 2, { ease: Bounce.easeOut, y: 0, scale: 1 });
+    });
 });
 
 // Gallery image dialogs
 const galleryImages = document.querySelectorAll('.dialog-overview');
 galleryImages.forEach((image, i) => {
-  const button = image.nextElementSibling;
-  button.addEventListener('click', () => {
-    image.show();
-  });
+    const button = image.nextElementSibling;
+    button.addEventListener('click', () => {
+        image.show();
+    });
 });
 
 // Gallery animations
 const galleryCard = gsap.utils.toArray(".card-basic");
 
 galleryCard.forEach((card, i) => {
-  card.addEventListener("mouseenter", (e) => {
-    gsap.to(e.target, duration / 4, { y: -10, scale: 1.2 });
-    gsap.to(e.target, { boxShadow: "0 0 20px rgba(0,0,0,0.06)" });
-  });
+    card.addEventListener("mouseenter", (e) => {
+        gsap.to(e.target, duration / 4, { y: -10, scale: 1.2 });
+        gsap.to(e.target, { boxShadow: "0 0 20px rgba(0,0,0,0.06)" });
+    });
 
-  card.addEventListener("mouseleave", (e) => {
-    gsap.to(e.target, duration / 2, { ease: Bounce.easeOut, y: 0, scale: 1 });
-  });
+    card.addEventListener("mouseleave", (e) => {
+        gsap.to(e.target, duration / 2, { ease: Bounce.easeOut, y: 0, scale: 1 });
+    });
 });
 
-// Pin each previously unpinned section to top of screen
-// const scrollPin = gsap.timeline({
-//   scrollTrigger: {
-//     trigger: ".pinSection",
-//     start: "top top",
-//     markers: true,
-//     end: "+=500",
-//     pin: true
-//   }
-// }) 
-
+// 
 gsap.utils.toArray('.pinSection').forEach(section => {
-  ScrollTrigger.create({
-    trigger: section,
-    markers: true,
-    refreshPriority: 0,
-    pin: true,
-    end: "+=500",
-  });
+    ScrollTrigger.create({
+        trigger: section,
+        markers: true,
+        refreshPriority: 0,
+        pin: true,
+        end: "+=500",
+    });
 });
 // With this enabled the DEMANTIA AMONG US section snapt to top of 
 //screen from bottom third
 
 // Skipping back up after completing animation of DEMENTUA AMONG US section
+
+// SVG arrow animation
+gsap.to(".downArrow", 0.5, { y: 10, repeat: -1, yoyo: true });
